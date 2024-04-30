d38471a38d0c03be29ec1937f6be345b08cb9ce2
nagginico <nagginico@gmail.com>
2024-04-30 14:37:07 -0300

    commit ""

--- shop_ropa/catalogo/views.py
+++ shop_ropa/catalogo/views.py
@@ -3,9 +3,9 @@ from .models import Producto
 
 def lista_productos(request):
     productos = Producto.objects.all()
-    return render(request, 'catalogo.html', {'productos': productos})
+    return render(request, 'catalogo/catalogo.html', {'productos': productos})
 
 def detalle_producto(request, producto_id):
     producto = get_object_or_404(Producto, pk=producto_id)
-    return render(request, 'detalleProducto.html', {'producto': producto})
+    return render(request, 'catalogo/detalleProducto.html', {'producto': producto})
 
